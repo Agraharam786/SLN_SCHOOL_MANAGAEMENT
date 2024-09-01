bc06e955dfc3076e5be9df2fc29d45b1fe064d05
Agraharam786 <162783231+Agraharam786@users.noreply.github.com>
2024-09-01 23:12:56 +0530

    Modified to Implement ALL functionality
    Modified to Implement ALL functionality

--- Database/Procedures/SLNSP_GETEXPENSEREPORT.sql
+++ Database/Procedures/SLNSP_GETEXPENSEREPORT.sql
@@ -1,9 +1,4 @@
 
-/*
-exec [SLNSP_GETEXPENSEREPORT] 0
-exec SLNSP_GETEXPENSEDETAILS 1
-*/
-GO
 /****** Object:  StoredProcedure [dbo].[SLNSP_GETEXPENSEREPORT]    Script Date: 27-Jul-24 10:13:40 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -22,7 +17,7 @@ AS
 BEGIN
  SET NOCOUNT ON; -- Helps to reduce network traffic and improve performance
  	
-	IF(@EXPENSE_TYPE <>'')
+	IF(@EXPENSE_TYPE <>'ALL')
 		BEGIN
 			SELECT 
 					EXPENSE_TYPE,				
@@ -40,8 +35,7 @@ BEGIN
 		BEGIN
 				SELECT 
 					EXPENSE_TYPE,				
-					SUM(EXPENSE_AMOUNT) EXPENSE_AMOUNT
-				
+					SUM(EXPENSE_AMOUNT) EXPENSE_AMOUNT				
 			FROM SLN_EXPENSE	
 			WHERE
 			ENTRY_DATE BETWEEN @FROM_DATE AND @TO_DATE
@@ -54,3 +48,10 @@ BEGIN
 END;
 
 GO
+
+/*
+
+	exec [SLNSP_GETEXPENSEREPORT] 'ALL','2024-08-01','2024-09-01'
+	exec [SLNSP_GETEXPENSEREPORT] 'Bus Maintanance','2024-08-01','2024-09-01'
+	
+*/
